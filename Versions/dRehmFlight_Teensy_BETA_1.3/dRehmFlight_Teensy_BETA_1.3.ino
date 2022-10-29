9bc0dfea9f8c4f5f0196c0ac58b6397d69084ecb
Christian <ch.braendli@gmail.com>
2022-10-30 00:35:41 +0200

    Print Format Fixed
    The Serial Print commands had a " " after the ":" and so they cannot be plotted. This was fixed

--- Versions/dRehmFlight_Teensy_BETA_1.3/dRehmFlight_Teensy_BETA_1.3.ino
+++ Versions/dRehmFlight_Teensy_BETA_1.3/dRehmFlight_Teensy_BETA_1.3.ino
@@ -396,7 +396,7 @@ void loop() {
   //Print data at 100hz (uncomment one at a time for troubleshooting) - SELECT ONE:
   //printRadioData();     //Prints radio pwm values (expected: 1000 to 2000)
   //printDesiredState();  //Prints desired vehicle state commanded in either degrees or deg/sec (expected: +/- maxAXIS for roll, pitch, yaw; 0 to 1 for throttle)
-  //printGyroData();      //Prints filtered gyro data direct from IMU (expected: ~ -250 to 250, 0 at rest)
+  printGyroData();      //Prints filtered gyro data direct from IMU (expected: ~ -250 to 250, 0 at rest)
   //printAccelData();     //Prints filtered accelerometer data direct from IMU (expected: ~ -2 to 2; x,y 0 when level, z 1 when level)
   //printMagData();       //Prints filtered magnetometer data direct from IMU (expected: ~ -300 to 300)
   //printRollPitchYaw();  //Prints roll, pitch, and yaw angles in degrees from Madgwick filter (expected: degrees, 0 when level)
@@ -515,7 +515,7 @@ void IMUinit() {
     if (status < 0) {
       Serial.println("MPU9250 initialization unsuccessful");
       Serial.println("Check MPU9250 wiring or try cycling power");
-      Serial.print("Status: ");
+      Serial.print("Status:");
       Serial.println(status);
       while(1) {}
     }
@@ -582,21 +582,23 @@ void getIMUdata() {
   GyroY_prev = GyroY;
   GyroZ_prev = GyroZ;
 
-  //Magnetometer
-  MagX = MgX/6.0; //uT
-  MagY = MgY/6.0;
-  MagZ = MgZ/6.0;
-  //Correct the outputs with the calculated error values
-  MagX = (MagX - MagErrorX)*MagScaleX;
-  MagY = (MagY - MagErrorY)*MagScaleY;
-  MagZ = (MagZ - MagErrorZ)*MagScaleZ;
-  //LP filter magnetometer data
-  MagX = (1.0 - B_mag)*MagX_prev + B_mag*MagX;
-  MagY = (1.0 - B_mag)*MagY_prev + B_mag*MagY;
-  MagZ = (1.0 - B_mag)*MagZ_prev + B_mag*MagZ;
-  MagX_prev = MagX;
-  MagY_prev = MagY;
-  MagZ_prev = MagZ;
+  #if defined USE_MPU9250_SPI
+    //Magnetometer
+    MagX = MgX/6.0; //uT
+    MagY = MgY/6.0;
+    MagZ = MgZ/6.0;
+    //Correct the outputs with the calculated error values
+    MagX = (MagX - MagErrorX)*MagScaleX;
+    MagY = (MagY - MagErrorY)*MagScaleY;
+    MagZ = (MagZ - MagErrorZ)*MagScaleZ;
+    //LP filter magnetometer data
+    MagX = (1.0 - B_mag)*MagX_prev + B_mag*MagX;
+    MagY = (1.0 - B_mag)*MagY_prev + B_mag*MagY;
+    MagZ = (1.0 - B_mag)*MagZ_prev + B_mag*MagZ;
+    MagX_prev = MagX;
+    MagY_prev = MagY;
+    MagZ_prev = MagZ;
+  #endif
 }
 
 void calculate_IMU_error() {
@@ -1552,17 +1554,17 @@ void setupBlink(int numBlinks,int upTime, int downTime) {
 void printRadioData() {
   if (current_time - print_counter > 10000) {
     print_counter = micros();
-    Serial.print(F(" CH1: "));
+    Serial.print(F(" CH1:"));
     Serial.print(channel_1_pwm);
-    Serial.print(F(" CH2: "));
+    Serial.print(F(" CH2:"));
     Serial.print(channel_2_pwm);
-    Serial.print(F(" CH3: "));
+    Serial.print(F(" CH3:"));
     Serial.print(channel_3_pwm);
-    Serial.print(F(" CH4: "));
+    Serial.print(F(" CH4:"));
     Serial.print(channel_4_pwm);
-    Serial.print(F(" CH5: "));
+    Serial.print(F(" CH5:"));
     Serial.print(channel_5_pwm);
-    Serial.print(F(" CH6: "));
+    Serial.print(F(" CH6:"));
     Serial.println(channel_6_pwm);
   }
 }
@@ -1570,13 +1572,13 @@ void printRadioData() {
 void printDesiredState() {
   if (current_time - print_counter > 10000) {
     print_counter = micros();
-    Serial.print(F("thro_des: "));
+    Serial.print(F("thro_des:"));
     Serial.print(thro_des);
-    Serial.print(F(" roll_des: "));
+    Serial.print(F(" roll_des:"));
     Serial.print(roll_des);
-    Serial.print(F(" pitch_des: "));
+    Serial.print(F(" pitch_des:"));
     Serial.print(pitch_des);
-    Serial.print(F(" yaw_des: "));
+    Serial.print(F(" yaw_des:"));
     Serial.println(yaw_des);
   }
 }
@@ -1584,11 +1586,11 @@ void printDesiredState() {
 void printGyroData() {
   if (current_time - print_counter > 10000) {
     print_counter = micros();
-    Serial.print(F("GyroX: "));
+    Serial.print(F("GyroX:"));
     Serial.print(GyroX);
-    Serial.print(F(" GyroY: "));
+    Serial.print(F(" GyroY:"));
     Serial.print(GyroY);
-    Serial.print(F(" GyroZ: "));
+    Serial.print(F(" GyroZ:"));
     Serial.println(GyroZ);
   }
 }
@@ -1596,11 +1598,11 @@ void printGyroData() {
 void printAccelData() {
   if (current_time - print_counter > 10000) {
     print_counter = micros();
-    Serial.print(F("AccX: "));
+    Serial.print(F("AccX:"));
     Serial.print(AccX);
-    Serial.print(F(" AccY: "));
+    Serial.print(F(" AccY:"));
     Serial.print(AccY);
-    Serial.print(F(" AccZ: "));
+    Serial.print(F(" AccZ:"));
     Serial.println(AccZ);
   }
 }
@@ -1608,11 +1610,11 @@ void printAccelData() {
 void printMagData() {
   if (current_time - print_counter > 10000) {
     print_counter = micros();
-    Serial.print(F("MagX: "));
+    Serial.print(F("MagX:"));
     Serial.print(MagX);
-    Serial.print(F(" MagY: "));
+    Serial.print(F(" MagY:"));
     Serial.print(MagY);
-    Serial.print(F(" MagZ: "));
+    Serial.print(F(" MagZ:"));
     Serial.println(MagZ);
   }
 }
@@ -1620,11 +1622,11 @@ void printMagData() {
 void printRollPitchYaw() {
   if (current_time - print_counter > 10000) {
     print_counter = micros();
-    Serial.print(F("roll: "));
+    Serial.print(F("roll:"));
     Serial.print(roll_IMU);
-    Serial.print(F(" pitch: "));
+    Serial.print(F(" pitch:"));
     Serial.print(pitch_IMU);
-    Serial.print(F(" yaw: "));
+    Serial.print(F(" yaw:"));
     Serial.println(yaw_IMU);
   }
 }
@@ -1632,11 +1634,11 @@ void printRollPitchYaw() {
 void printPIDoutput() {
   if (current_time - print_counter > 10000) {
     print_counter = micros();
-    Serial.print(F("roll_PID: "));
+    Serial.print(F("roll_PID:"));
     Serial.print(roll_PID);
-    Serial.print(F(" pitch_PID: "));
+    Serial.print(F(" pitch_PID:"));
     Serial.print(pitch_PID);
-    Serial.print(F(" yaw_PID: "));
+    Serial.print(F(" yaw_PID:"));
     Serial.println(yaw_PID);
   }
 }
@@ -1644,17 +1646,17 @@ void printPIDoutput() {
 void printMotorCommands() {
   if (current_time - print_counter > 10000) {
     print_counter = micros();
-    Serial.print(F("m1_command: "));
+    Serial.print(F("m1_command:"));
     Serial.print(m1_command_PWM);
-    Serial.print(F(" m2_command: "));
+    Serial.print(F(" m2_command:"));
     Serial.print(m2_command_PWM);
-    Serial.print(F(" m3_command: "));
+    Serial.print(F(" m3_command:"));
     Serial.print(m3_command_PWM);
-    Serial.print(F(" m4_command: "));
+    Serial.print(F(" m4_command:"));
     Serial.print(m4_command_PWM);
-    Serial.print(F(" m5_command: "));
+    Serial.print(F(" m5_command:"));
     Serial.print(m5_command_PWM);
-    Serial.print(F(" m6_command: "));
+    Serial.print(F(" m6_command:"));
     Serial.println(m6_command_PWM);
   }
 }
@@ -1662,19 +1664,19 @@ void printMotorCommands() {
 void printServoCommands() {
   if (current_time - print_counter > 10000) {
     print_counter = micros();
-    Serial.print(F("s1_command: "));
+    Serial.print(F("s1_command:"));
     Serial.print(s1_command_PWM);
-    Serial.print(F(" s2_command: "));
+    Serial.print(F(" s2_command:"));
     Serial.print(s2_command_PWM);
-    Serial.print(F(" s3_command: "));
+    Serial.print(F(" s3_command:"));
     Serial.print(s3_command_PWM);
-    Serial.print(F(" s4_command: "));
+    Serial.print(F(" s4_command:"));
     Serial.print(s4_command_PWM);
-    Serial.print(F(" s5_command: "));
+    Serial.print(F(" s5_command:"));
     Serial.print(s5_command_PWM);
-    Serial.print(F(" s6_command: "));
+    Serial.print(F(" s6_command:"));
     Serial.print(s6_command_PWM);
-    Serial.print(F(" s7_command: "));
+    Serial.print(F(" s7_command:"));
     Serial.println(s7_command_PWM);
   }
 }
@@ -1682,7 +1684,7 @@ void printServoCommands() {
 void printLoopRate() {
   if (current_time - print_counter > 10000) {
     print_counter = micros();
-    Serial.print(F("dt = "));
+    Serial.print(F("dt:"));
     Serial.println(dt*1000000.0);
   }
 }
